5d24bcae319ff12c0974ec3aab55d415b7eb05d6
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-09 19:47:36 +0300

    refactoring

--- app/src/main/java/com/umbrella/compose/MainActivity.kt
+++ app/src/main/java/com/umbrella/compose/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.umbrella.compose
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.umbrella.compose.ui.activities.visibility.VisibilityActivity
 import com.umbrella.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,46 +22,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column(
-                        modifier = Modifier.fillMaxHeight().fillMaxWidth(),
-                        verticalArrangement = Arrangement.Top,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        makeButton(this@MainActivity::makeToast, "Example 1")
-                        makeButton(this@MainActivity::makeToast, "Example 2")
-                        makeButton(this@MainActivity::makeToast, "Example 3")
-                        makeButton(this@MainActivity::makeToast, "Example 4")
-                        makeButton(this@MainActivity::makeToast, "Example 5")
-                    }
-                }
+                createNavigation()
             }
         }
     }
 
-    private fun makeToast() {
-        Toast.makeText(this, this.javaClass.toString(), Toast.LENGTH_SHORT).show()
+    @Composable
+    fun createNavigation() {
+        Surface(
+            color = MaterialTheme.colors.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                makeButton({
+                    startActivity(
+                        Intent(
+                            applicationContext,
+                            VisibilityActivity::class.java
+                        )
+                    )
+                }, "Visibility activity")
+            }
+        }
     }
-}
 
-@Composable
-fun makeButton(onClick: () -> Unit, text: String) {
-    Button(
-        onClick = { onClick.invoke() },
-        modifier = Modifier.height(60.dp).fillMaxWidth()
-            .padding(start = 10.dp, end = 10.dp, top = 10.dp)
-    ) {
-        Text(text = text)
+    @Composable
+    fun makeButton(onClick: () -> Unit, text: String) {
+        Button(
+            onClick = { onClick.invoke() },
+            modifier = Modifier.height(60.dp).fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp, top = 10.dp)
+        ) {
+            Text(text = text)
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        makeButton({}, "ASDAS")
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposeTheme {
+            createNavigation()
+        }
     }
 }
