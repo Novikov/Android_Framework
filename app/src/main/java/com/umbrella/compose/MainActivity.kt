14430f108534f3a127a8b509bc722deeb8373cca
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-09 16:58:57 +0300

    activity template tmp

--- app/src/main/java/com/umbrella/compose/MainActivity.kt
+++ app/src/main/java/com/umbrella/compose/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.umbrella.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.umbrella.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +22,56 @@ class MainActivity : ComponentActivity() {
             ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                        makeButton { makeToast() }
+                    }
                 }
             }
         }
     }
+
+    private fun makeToast() {
+        Toast.makeText(this, this.javaClass.toString(), Toast.LENGTH_SHORT).show()
+    }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun makeButton(onClick: () -> Unit) {
+    Button(
+        onClick = { onClick.invoke() },
+        modifier = Modifier.height(60.dp).fillMaxWidth()
+            .padding(start = 10.dp, end = 10.dp, top = 10.dp)
+    ) {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTheme {
-        Greeting("Android")
+        makeButton {}
     }
 }
