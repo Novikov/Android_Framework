8f6f9cec0dee2ea824c7ead91155e26988a6e6dc
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-17 12:09:42 +0300

    expanded savepoinit

--- app/src/main/java/com/umbrella/compose/ui/activities/cards_list/ListWithVisibilityActivity.kt
+++ app/src/main/java/com/umbrella/compose/ui/activities/cards_list/ListWithVisibilityActivity.kt
@@ -7,9 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,19 +44,31 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
+    // !!! Каждый раз при вызове функции будет создаваться копия данного state
+    val expanded = remember { mutableStateOf(false) }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = "$name!")
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(
+                    text = if (expanded.value) "Show less" else "Show more",
+                    color = MaterialTheme.colors.primary
+                )
             }
         }
     }
