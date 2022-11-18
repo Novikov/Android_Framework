6c2cb8fd66c49890a2376626082b620e74ec83e4
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-18 17:01:57 +0300

    savepoint

--- app/src/main/java/com/umbrella/compose/ui/activities/state/StateListActivity.kt
+++ app/src/main/java/com/umbrella/compose/ui/activities/state/StateListActivity.kt
@@ -11,9 +11,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,9 +45,14 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = rememberSaveable{ mutableStateOf(0) }
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+
+        if (count > 0) {
+            // This text is present if the button has been clicked
+            // at least once; absent otherwise
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
