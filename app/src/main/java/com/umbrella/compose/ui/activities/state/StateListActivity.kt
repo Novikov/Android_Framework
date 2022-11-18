0a274839545c1cd528d1cc6ff8c0b22986a86f18
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-18 16:43:20 +0300

    screen rotation after configuration changes

--- app/src/main/java/com/umbrella/compose/ui/activities/state/StateListActivity.kt
+++ app/src/main/java/com/umbrella/compose/ui/activities/state/StateListActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,8 +44,8 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember {mutableStateOf(0)}
-        Text("You've had $count glasses.")
+        val count: MutableState<Int> = rememberSaveable{ mutableStateOf(0) }
+        Text("You've had ${count.value} glasses.")
         Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
