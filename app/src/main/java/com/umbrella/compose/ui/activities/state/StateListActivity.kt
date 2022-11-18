c82d806a5a65a576e8c2fcaf79a7d664dae525f5
Igor Novikov <igor.novikov@umbrellait.com>
2022-11-18 16:39:03 +0300

    savepoint

--- app/src/main/java/com/umbrella/compose/ui/activities/state/StateListActivity.kt
+++ app/src/main/java/com/umbrella/compose/ui/activities/state/StateListActivity.kt
@@ -3,13 +3,20 @@ package com.umbrella.compose.ui.activities.state
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.umbrella.compose.ui.activities.state.ui.theme.ComposeTheme
 
 class StateListActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class StateListActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting2("Android")
+                    WellnessScreen()
                 }
             }
         }
@@ -30,14 +37,25 @@ class StateListActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        val count: MutableState<Int> = remember {mutableStateOf(0)}
+        Text("You've had $count glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     ComposeTheme {
-        Greeting2("Android")
+        WellnessScreen()
     }
 }
