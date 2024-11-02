c5982686dcd4365549f77e55e766caea0341c321
Novikov <abovefortune@gmail.com>
2024-11-02 11:02:04 +0300

    refactor

--- os_android/src/main/java/com/app/ipc/Plan.kt
+++ os_android/src/main/java/com/app/ipc/Plan.kt
@@ -1,7 +1,14 @@
-package com.app.publishing
+package com.app.ipc
 
 /**
  * Способы публикации приложения
  * Анализ APK
  * Обфускация и оптимизация APK
+ *
+ *
+ * IPC
+ * Binder
+ *
+ * Dalvik vs Art
+ * Jit компиляция итд
  * */
